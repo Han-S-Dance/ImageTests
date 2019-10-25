ed0fd00495c527b91a9cdeb83ab5f8aa8096340c
Han Dance <hdance24@gmail.com>
2019-10-25 13:40:28 +0100

    added user ctor and allocation of pixel buffer

--- Image.cpp
+++ Image.cpp
@@ -1,6 +1,14 @@
 
 #include "Image.h"
 
+Image::Image(unsigned int _width, unsigned int _height, unsigned int _channels)
+{
+  m_width=_width;
+  m_height=_height;
+  m_channels=_channels;
+  m_pixels= std::make_unique<unsigned char []>(m_width*m_height*m_channels);
+}
+
 unsigned int Image::width() const noexcept
 {
     return m_width;
@@ -14,4 +22,9 @@ unsigned int Image::height() const noexcept
 unsigned int Image::channels() const noexcept
 {
     return m_channels;
+}
+
+unsigned char * Image::pixels()
+{
+    return m_pixels.get();
 }
