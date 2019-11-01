1a76a9f48d6e9fc44bb65c2262a8ccaa5aeaaa2a
Han Dance <hdance24@gmail.com>
2019-11-01 12:17:00 +0000

    commit befoe refactor of project

--- ImageTests.cpp
+++ ImageTests.cpp
@@ -38,3 +38,38 @@ TEST(Image,setPixel)
     EXPECT_EQ(g,128);
     EXPECT_EQ(b,64);
 }
+
+TEST(image,writeFile)
+{
+    Image i(100,100);
+    i.setPixel(0,0,255,0,0);
+    i.setPixel(1,0,0,255,0);
+    i.setPixel(2,0,0,0,255);
+    EXPECT_TRUE(i.write("test.png"));
+}
+
+
+TEST(Image,clearColour)
+{
+    Image i(100,100);
+    i.clearColour(128,128,128);
+    i.setPixel(1,0,0,255,0);
+    i.setPixel(2,0,0,0,255);
+    EXPECT_TRUE(i.write("test.png"));
+}
+
+TEST(Image,copyctor)
+{
+    Image i(100,100);
+    i.clearColour(128,128,128);
+    i.setPixel(0,0,255,0,0);
+    i.setPixel(1,0,0,255,0);
+    i.setPixel(2,0,0,0,255);
+
+    Image image2=i;
+    unsigned char r,g,b;
+    i.getPixel(0,0,r,g,b);
+    EXPECT_EQ(r,255);
+    EXPECT_EQ(g,0);
+    EXPECT_EQ(b,0);
+}
